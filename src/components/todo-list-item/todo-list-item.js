b7d7fc89dc74550e0016c34f4d7d6e337d7eaf3a
Nikita M <nikitama1hr@gmail.com>
2020-09-20 14:05:54 +0300

    Add class done Add logic at click on label done

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -3,17 +3,30 @@ import React, {Component} from 'react';
 import './todo-list-item.css';
 
 class TodoListItem extends Component {
+    state = {
+      done: false
+    }
+    clickLable=()=>{this.setState({
+        done: true
+    })}
     render() {
         const {label, important = false} = this.props
+        const {done} = this.state
+
+        let classNames = 'todo-list-item'
+    if (done){
+        classNames +=' done'
+    }
         const style = {
             color: important ? 'steelblue' : 'black',
             fontWeight: important ? 'bold' : 'normal'
         };
         return (
-            <span className="todo-list-item">
+            <span className={classNames}>
       <span
           className="todo-list-item-label"
-          style={style}>
+          style={style}
+      onClick={this.clickLable}>
         {label}
       </span>
 
