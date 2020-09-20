cd4024db7b3130c85b1fe9aa9c10aa828a81b7b6
Nikita M <nikitama1hr@gmail.com>
2020-09-20 16:32:06 +0300

    task entry in todo list

--- src/components/item-add-form/item-add-form.js
+++ src/components/item-add-form/item-add-form.js
@@ -1,11 +1,29 @@
 import React, {Component} from 'react';
 import './item-add-form.css'
-export default class ItemAddForm extends Component{
-render() {
-    return(
-        <div className="item-add-form">
-            <button className="btn btn-warning" onClick={()=>{this.props.addItem('Hello World')}}>Add item</button>
-        </div>
-    )
-}
+
+export default class ItemAddForm extends Component {
+    state = {
+        label: ''
+    }
+    onLabelChange = (e) => {
+        this.setState({
+            label: e.target.value
+        })
+    }
+    onSubmit = (e) => {
+        e.preventDefault()
+        this.props.onItemAdded(this.state.label)
+    }
+
+    render() {
+        return (
+            <form className="item-add-form d-flex" onSubmit={this.onSubmit}>
+
+                <input type="text" className="form-control" onChange={this.onLabelChange}
+                       placeholder="Add task on list.."/>
+                <button className="btn btn-warning">Add item
+                </button>
+            </form>
+        )
+    }
 }
