785c4d0fdd9b0a71069183fdee6c874c1114c51f
Nik <nikitama1hr@gmail.com>
2023-04-09 00:53:02 +0300

    Update package.json && edit setting

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from "./components/app/index";
-
-ReactDOM.render(<App />,
-  document.getElementById('root'));
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const react_1 = __importDefault(require("react"));
+const react_dom_1 = __importDefault(require("react-dom"));
+const index_js_1 = __importDefault(require("./components/app/index.js"));
+react_dom_1.default.render(react_1.default.createElement(index_js_1.default), document.getElementById('root'));
