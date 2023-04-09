ff225840e3fa8373b0c156863a8cbc5ec17b625c
Nik <nikitama1hr@gmail.com>
2023-04-09 08:46:05 +0300

    Refactor todo-list-item JSX -> TSX

--- src/components/todo-list-item/index.js
+++ src/components/todo-list-item/index.js
@@ -1,2 +1,7 @@
-import TodoListItem from "./todo-list-item";
-export default TodoListItem
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const todo_list_item_1 = __importDefault(require("./todo-list-item"));
+exports.default = todo_list_item_1.default;
---
+++ src/index.tsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from "./components/app/index.js";
+
+ReactDOM.render(React.createElement(App),
+  document.getElementById('root'));
